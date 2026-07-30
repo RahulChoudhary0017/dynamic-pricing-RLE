f6f0ef4a46936352b5c4beed60b137d4732b758b
RahulChoudhary0017 <rahulchoudhary8037@gmail.com>
2026-07-30 20:19:26 +0530

    Complete Day 11: State discretization and Q-Learning optimization

--- env/pricing_env.py
+++ env/pricing_env.py
@@ -28,8 +28,8 @@ class PricingEnvironment(gym.Env):
         # Observation Space
         # State = [Remaining Inventory, Days Left]
         self.observation_space = spaces.Box(
-            low=np.array([0, 0]),
-            high=np.array([100, 30]),
+            low=np.array([0, 0, 0]),
+            high=np.array([100, 30, 2]),
             dtype=np.int32
         )
 
@@ -41,7 +41,7 @@ class PricingEnvironment(gym.Env):
 
         self.inventory = self.max_inventory
         self.days_left = self.max_days
-        self.competitor_price = 3000
+        self.competitor_price = np.random.randint(2500, 4501)
 
         state = np.array(
             [self.inventory, self.days_left],
@@ -53,12 +53,17 @@ class PricingEnvironment(gym.Env):
     def step(self, action):
 
      price = PRICE_MAP[action] 
-     
-     import random
+       # Competitor price changes every day
+     self.competitor_price += np.random.randint(-100, 101)
 
-     self.competitor_price = random.choice(
-     [2000,2500,3000,3500,4000]
-    )
+       # Keep competitor price in range
+     self.competitor_price = int(
+         np.clip(
+             self.competitor_price,
+             2500,
+             4500
+         )
+     )
 
      # Customer Demand
      sold_rooms = customer_demand(action, self.days_left)
@@ -110,10 +115,22 @@ class PricingEnvironment(gym.Env):
      if self.inventory == 0 or self.days_left == 0:
         done = True
 
+     if self.competitor_price < 2800:
+        competitor_level = 0      # Low
+     elif self.competitor_price < 3600:
+        competitor_level = 1      # Medium
+     else:
+         competitor_level = 2      # High
+
      state = np.array(
-        [self.inventory, self.days_left],
-        dtype=np.int32
-    )
+    [
+        self.inventory,
+        self.days_left,
+        competitor_level
+    ],
+    dtype=np.int32
+)
+
 
      return state, reward, done, False, {}
 
